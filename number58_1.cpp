bc5b55e2e4c0ad916f5f6470fa42422ac4e2652a
Hansol <hansol4412@naver.com>
2020-08-24 18:29:06 +0900

    number58_1

---
+++ number58_1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+int DFS(int n,int x){
+	if(x>n){
+		return 0;
+	}
+	else{
+		printf("%d",x);
+		DFS(n,2*x);
+		DFS(n,2*x+1);
+	}
+}
+int main() { 
+// 58. 이진트리 깊이우선트리(DFS)
+// 전위순회 출력.
+	int n;
+	printf("노드의 갯수를 입력하세요:");
+	scanf("%d",&n);
+	DFS(n,1);
+	return 0;
+ 
+}
