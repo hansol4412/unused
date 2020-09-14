14a6ef899805247c9b1a4de0d209a24525f490f8
Hansol <hansol4412@naver.com>
2020-09-14 09:51:16 +0900

    number63_1

---
+++ Number63_1.java
@@ -0,0 +1,31 @@
+package algorithm;
+import java.util.Scanner;
+public class Number63_1 {
+
+	public static void main(String[] args) {
+		// 63_1. 인접행렬 (방향그래프)
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("정점의 수를 입력하세요:");
+		int n = stdIn.nextInt();
+		System.out.print("간선의 수를 입력하세요:");
+		int v = stdIn.nextInt();
+		int[][] a = new int[n+1][n+1];
+		int i;
+		int o;
+		for(int k =0; k<v; k++) {
+			i = stdIn.nextInt();
+			o = stdIn.nextInt();
+			a[i][o]=1;
+		}
+		
+		for(int k=1; k<=n; k++) {
+			for(int j=1; j<=n; j++) {
+				System.out.print(a[k][j]+ "");
+			}
+			System.out.println();
+			}
+		
+
+	}
+
+}
